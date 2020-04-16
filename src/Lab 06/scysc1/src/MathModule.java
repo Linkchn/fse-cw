be1fc1120726ec3c7887086484d6280bb8783c0a
Colin <csl122@hotmail.com>
2020-04-16 14:25:44 +0800

    up on scysc1

--- src/Lab 06/scysc1/src/MathModule.java
+++ src/Lab 06/scysc1/src/MathModule.java
@@ -2,9 +2,39 @@ public class MathModule {
 
     public static void main(String[] args) {
 
+        try {
+            int test = myMultiply(2000000000, 6);
+            System.out.println(test);
+        } catch(Exception e) {
+            System.out.println(e.toString());
+        }
+        try {
+            int test = myDivide(2000000000, 0);
+            System.out.println(test);
+        } catch(Exception e) {
+            System.out.println(e.toString());
+        }
+
+
     }
 
-    public static int myMultiply(int firstNum, int secondNum) {
+    public static int myMultiply(int firstNum, int secondNum) throws Exception {
+        long newAnswer = (long) firstNum * secondNum;
+        if (newAnswer > Integer.MAX_VALUE || newAnswer < Integer.MIN_VALUE) {
+            throw new Exception("Number too big");
+        }
         return firstNum * secondNum;
+    }
+
+    public static int myDivide(int firstNum, int secondNum) throws Exception {
+        long newAnswer = (long) firstNum / secondNum;
+        if (newAnswer > Integer.MAX_VALUE || newAnswer < Integer.MIN_VALUE) {
+            throw new Exception("Number too big");
+        }
+        else if (firstNum % secondNum != 0)
+        {
+            throw new Exception("Cannot be divided fully");
+        }
+        return firstNum / secondNum;
     }
 }
--- src/Lab 06/scysc1/src/MathModuleTest.java
+++ src/Lab 06/scysc1/src/MathModuleTest.java
@@ -6,18 +6,70 @@ class MathModuleTest {
 
     static int input1 = 0;
     static int input2 = 0;
+    static int input3 = 0;
+    static int input4 = 0;
 
     @BeforeAll
     static void setup() {
         input1 = 3;
-        input2 = 6;
+        input2 = 7;
+        input3 = 2000000000;
+        input4 = 0;
     }
 
     @Test
     void test1() {
-        int myAnswer = MathModule.myMultiply(input1, input2);
-        assertEquals(18, myAnswer);
+        int myAnswer = 0;
+        try {
+            myAnswer = MathModule.myMultiply(input1, input2);
+        } catch(Exception e) { }
+        assertEquals(21, myAnswer);
     }
+
+    @Test
+    void testError() {
+        int myAnswer = 0;
+        try {
+            myAnswer = MathModule.myMultiply(input2, input3);
+        } catch(Exception e) {
+            if(e.getClass() == Exception.class) {
+                return; // it passed
+            }
+        }
+        fail("it failed");
+
+    }
+
+    @Test
+    void testAssertThrows() {
+        assertThrows(Exception.class, ()->{MathModule.myMultiply(input2, input3);});
+    }
+
+    @Test
+    void test2() {
+        int myAnswer = 0;
+        try {
+            myAnswer = MathModule.myDivide(input2, input1);
+        } catch(Exception e) {
+            assertThrows(Exception.class, ()->{
+                MathModule.myDivide(input2, input1);
+            });
+        }
+        assertEquals(0, myAnswer);
+    }
+
+    @Test
+    void testDividedBy0() {
+        int myAnswer = 0;
+        try {
+            myAnswer = MathModule.myDivide(input2, input4);
+        } catch(Exception e) {
+            assertThrows(ArithmeticException.class, ()->{
+                MathModule.myDivide(input2, input4);
+            });
+        }
+    }
+
 
 
 }
