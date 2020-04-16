7f8d886bee64fdfd3e284c62a50a963e05d47bd3
Haonan CHEN <scyhc3@nottingham.edu.cn>
2020-04-16 08:10:45 +0000

    Upload New File

---
+++ src/HaonanCHEN/TestMyMathModule.java
@@ -0,0 +1,65 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Ignore;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class TestMyMathModule {
+	private static int input1, input2, input3;
+	@BeforeAll 
+	static void setup() {
+		input1 = 3;  
+		input2 = 6; 
+		input3 = 2000000000;
+	}
+	
+	@Ignore
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+//	void test1() throws Exception {
+//		int myAnswer = 0;
+//		try {
+//			myAnswer=MathModule.myMultiply(input2, input3);
+//		}catch(Exception e) {
+//			
+//		}
+//		assertEquals(12000000000, myAnswer); 
+//	}
+	
+//	void testError() {  
+//		int myAnswer = 0;
+//		try {   
+//			myAnswer = MathModule.myMultiply(input2, input3);
+//		} catch(Exception e) {   
+//			if(e.getClass() == Exception.class) {    
+//				return; // it passed   
+//			} 
+//		} 
+//		fail("it failed"); 
+//	}
+	
+	void test2() throws Exception {
+		double myAnswer = 0;
+		try {
+			myAnswer=MathModule.myDivsion(20, 3);
+		}catch(Exception e) {
+			
+		}
+		assertEquals(6.67, myAnswer); 
+	}
+		
+//	void testError() {  
+//		double myAnswer = 0;
+//		try {   
+//			myAnswer = MathModule.myDivsion(20, 1);
+//		} catch(Exception e) {   
+//			if(e.getClass() == Exception.class) {    
+//				return; // it passed   
+//			} 
+//		} 
+//		fail("it failed"); 
+//	}
+}
