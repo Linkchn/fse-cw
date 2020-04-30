0b05882c19ad0a23ed962e17653897badc5d4df3
Colin <csl122@hotmail.com>
2020-04-30 22:34:51 +0800

    updated function 8 tests

--- src/CW3/src/BoCCategoryTest.java
+++ src/CW3/src/BoCCategoryTest.java
@@ -14,11 +14,13 @@ class BoCCategoryTest {
 
     static BoCCategory cat1;
     static BigDecimal bd1;
+    static BigDecimal sum;
 
     @BeforeAll
     static void set() {
         cat1 = new BoCCategory();
         bd1 = new BigDecimal("100");
+        sum = new BigDecimal("0.00");
     }
 
     @BeforeEach
@@ -33,10 +35,11 @@ class BoCCategoryTest {
     @DisplayName("addExpenseTest")
     @ParameterizedTest
     @ValueSource(strings = {"0.00", "100.00", "111.50", "10000000000"})
-    void addExpenseTest() {
-        bd1 = new BigDecimal("100");
+    void addExpenseTest(String num) {
+        bd1 = new BigDecimal(num);
+        sum = sum.add(bd1);
         cat1.addExpense(bd1);
-        assertEquals(bd1, cat1.CategorySpend());
+        assertEquals(sum, cat1.CategorySpend());
     }
 
     @Test
