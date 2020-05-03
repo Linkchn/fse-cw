368062057c9c2a19c655182edd87a0d4e0b48831
Colin <csl122@hotmail.com>
2020-05-04 00:52:20 +0800

    updated  BoCAppTest

--- src/CW3/src/BoCAppTest.java
+++ src/CW3/src/BoCAppTest.java
@@ -2,16 +2,29 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.*;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 
 class BoCAppTest {
 
+    private static ArrayList<BoCTransaction> UserTransactions;
+    private static ArrayList<BoCCategory> UserCategories;
+    private static Scanner in;
+    private static BoCApp a = null;
+
     @BeforeAll 
     static void setup() {
-
+        UserCategories = new ArrayList<BoCCategory>();
+        UserTransactions = new ArrayList<BoCTransaction>();
+        UserCategories.add(new BoCCategory("Unknown"));
+        in = new Scanner(System.in);
+        a = new BoCApp();
     }
 
     @BeforeEach
@@ -30,32 +43,37 @@ class BoCAppTest {
     }
 
     @Test
-    void listTransactions() {
+    void listTransactionsTest() {
 
     }
 
     @Test
-    void categoryOverview() {
+    void categoryOverviewTest() {
         
     }
 
     @Test
-    void listTransactionsForCategory() {
+    void listTransactionsForCategoryTest() {
 
     }
 
     @Test
-    void ChangeTransactionCategory() {
+    void ChangeTransactionCategoryTest() {
 
     }
 
     @Test
-    void AddTransaction() {
-
+    void AddTransactionTest() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method AddTransactionTest = a.getClass().getDeclaredMethod("AddTransaction", Scanner.class);
+        AddTransactionTest.setAccessible(true);
+        //调用
+        Object result = AddTransactionTest.invoke(a, "this is a test information");
+        System.out.println(result);
+        assertNotNull(result);
     }
 
     @Test
-    void AddCategory() {
+    void AddCategoryTest() {
 
     }
 }
