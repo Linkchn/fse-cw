c6dc129015b1768d3c38a516375513c366af602e
Daze_2 <scyhp1@nottingham.edu.cn>
2020-05-05 21:44:05 +0800

    Fix Test file in master 2

--- src/CW3/src/BoCCategoryTest.java
+++ src/CW3/src/BoCCategoryTest.java
@@ -2,8 +2,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.math.BigDecimal;
 
-import org.junit.Ignore;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
