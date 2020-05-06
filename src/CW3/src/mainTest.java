88dc0b2d89a381d981ad2d2fb788abbe5854a0be
Daze_2 <scyhp1@nottingham.edu.cn>
2020-05-06 20:51:07 +0800

    fix test

--- src/CW3/src/mainTest.java
+++ src/CW3/src/mainTest.java
@@ -54,7 +54,7 @@ class mainTest {
                         "3) Groceries(¥75.00) - Est. ¥31.00 (¥44.00 Remaining)\r\n" +
                         "4) Social(¥100.00) - Est. ¥22.49 (¥77.51 Remaining)\r\n" +
                         "\nWhat do you want to do?\n" +
-                        " T = List All [T]ransactions, [num] = Show Category [num], A = [A]dd Transaction, X = E[x]it\r\n" +
+                        "O = [O]verview\nT = List All [T]ransactions\n[num] = Show Category [num]\nC = [C]hange Transaction Category\nA = [A]dd Transaction\nN = [N]ew Category\nX = E[x]it\r\n" +
                         "Goodbye!\r\n"),
                 Arguments.arguments("N\nTestTitle\n6.23\nX\n",
                 		"1) Unknown(¥0.00) - Est. ¥850.00 (¥850.00 Overspent)\r\n" + 
@@ -63,7 +63,7 @@ class mainTest {
                 		"4) Social(¥100.00) - Est. ¥22.49 (¥77.51 Remaining)\r\n" + 
                 		"\n" + 
                 		"What do you want to do?\n" + 
-                		" T = List All [T]ransactions, [num] = Show Category [num], A = [A]dd Transaction, X = E[x]it\r\n" + 
+                		"O = [O]verview\nT = List All [T]ransactions\n[num] = Show Category [num]\nC = [C]hange Transaction Category\nA = [A]dd Transaction\nN = [N]ew Category\nX = E[x]it\r\n" + 
                 		"What is the title of the category?\r\n" + 
                 		"NOTE: It should not be blank and should be at most 15 characters.\r\n" + 
                 		"What is the budget for this category?\r\n" + 
@@ -76,7 +76,7 @@ class mainTest {
                 		"5) TestTitle(¥6.23) - Est. ¥0.00 (¥6.23 Remaining)\r\n" + 
                 		"\n" + 
                 		"What do you want to do?\n" + 
-                		" O = [O]verview, T = List All [T]ransactions, [num] = Show Category [num], C = [C]hange Transaction Category, A = [A]dd Transaction, N = [N]ew Category, X = E[x]it\r\n" + 
+                		"O = [O]verview\nT = List All [T]ransactions\n[num] = Show Category [num]\nC = [C]hange Transaction Category\nA = [A]dd Transaction\nN = [N]ew Category\nX = E[x]it\r\n" + 
                 		"Goodbye!\r\n" 
                 		)
 //                Arguments.arguments(""),
