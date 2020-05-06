23dc4f3d356cc021a08931c3e20e39660a7baec7
JiaweiWu <scyjw2@nottingham.edu.cn>
2020-05-06 20:23:26 +0800

    modify test code

--- src/CW3/src/mainTest.java
+++ src/CW3/src/mainTest.java
@@ -55,7 +55,29 @@ class mainTest {
                         "4) Social(¥100.00) - Est. ¥22.49 (¥77.51 Remaining)\r\n" +
                         "\nWhat do you want to do?\n" +
                         " T = List All [T]ransactions, [num] = Show Category [num], A = [A]dd Transaction, X = E[x]it\r\n" +
-                        "Goodbye!\r\n")
+                        "Goodbye!\r\n"),
+                Arguments.arguments("N\nTestTitle\n6.23\nX\n","1) Unknown(¥0.00) - Est. ¥850.00 (¥850.00 Overspent)\r\n" + 
+                		"2) Bills(¥120.00) - Est. ¥112.99 (¥7.01 Remaining)\r\n" + 
+                		"3) Groceries(¥75.00) - Est. ¥31.00 (¥44.00 Remaining)\r\n" + 
+                		"4) Social(¥100.00) - Est. ¥22.49 (¥77.51 Remaining)\r\n" + 
+                		"\n" + 
+                		"What do you want to do?\n" + 
+                		" T = List All [T]ransactions, [num] = Show Category [num], A = [A]dd Transaction, X = E[x]it\r\n" + 
+                		"What is the title of the category?\r\n" + 
+                		"NOTE: It should not be blank and should be at most 15 characters.\r\n" + 
+                		"What is the budget for this category?\r\n" + 
+                		"Note:It should be a pisitive decimal number with exact two decimal places.\r\n" + 
+                		"[Category added]\r\n" + 
+                		"1) Unknown(¥0.00) - Est. ¥850.00 (¥850.00 Overspent)\r\n" + 
+                		"2) Bills(¥120.00) - Est. ¥112.99 (¥7.01 Remaining)\r\n" + 
+                		"3) Groceries(¥75.00) - Est. ¥31.00 (¥44.00 Remaining)\r\n" + 
+                		"4) Social(¥100.00) - Est. ¥22.49 (¥77.51 Remaining)\r\n" + 
+                		"5) TestTitle(¥6.23) - Est. ¥0.00 (¥6.23 Remaining)\r\n" + 
+                		"\n" + 
+                		"What do you want to do?\n" + 
+                		" O = [O]verview, T = List All [T]ransactions, [num] = Show Category [num], C = [C]hange Transaction Category, A = [A]dd Transaction, N = [N]ew Category, X = E[x]it\r\n" + 
+                		"Goodbye!\r\n" 
+                		)
 //                Arguments.arguments(""),
 //                Arguments.arguments("")
         );
