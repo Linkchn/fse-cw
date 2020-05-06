f7b4552a497b5e4b594a5d5adcc0807f605353ba
Yukai Lu <scyyl6@nottingham.edu.cn>
2020-05-06 21:19:25 +0800

    Code of mainTest5 has been finished.

--- src/CW3/src/mainTest.java
+++ src/CW3/src/mainTest.java
@@ -36,17 +36,26 @@ class mainTest {
         outContent.reset();
     }
     
+    /*
+    5 – Pass – Leo - 21:18/6/5  
+	Problem: /
+	Reason:/
+	Traceability: mainTest5
+     */
     @DisplayName("mainTest5")
     @ParameterizedTest
     @MethodSource
-    void mainTest5(String a, String b) {
+    void mainTest5(String a, String b, String c) {
         String input = a;
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
+        long timeStamp = System.currentTimeMillis(); 
+		SimpleDateFormat sdff=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String sd = sdff.format(new Date(timeStamp));
         BoCApp.main(new String[]{""});
-        assertEquals(b, outContent.toString());
+        assertEquals(b + sd + "\r\n" + c, outContent.toString());
     }
-    static List<Arguments> mainTest() {
+    static List<Arguments> mainTest5() {
         return List.of( // arguments:
                 Arguments.arguments("C\n1\n3\nX\n","1) Unknown(¥0.00) - Est. ¥850.00 (¥850.00 Overspent)\r\n" + 
                 		"2) Bills(¥120.00) - Est. ¥112.99 (¥7.01 Remaining)\r\n" + 
@@ -56,7 +65,7 @@ class mainTest {
                 		"What do you want to do?\n" + 
                 		" T = List All [T]ransactions, [num] = Show Category [num], A = [A]dd Transaction, X = E[x]it\r\n" +
                 		"Which transaction ID?\r\n" + 
-                		"	- Rent - ¥850.00		2020-05-06 20:08:26\r\n" + 
+                		"	- Rent - ¥850.00		", 
                 		"Which category will it move to?\r\n" + 
                 		"1) Unknown(¥0.00) - Est. ¥850.00 (¥850.00 Overspent)\r\n" + 
                 		"2) Bills(¥120.00) - Est. ¥112.99 (¥7.01 Remaining)\r\n" + 
@@ -65,8 +74,8 @@ class mainTest {
                 		"Change complete!\r\n" + 
                 		"Target category: 3) Groceries(¥75.00) - Est. ¥881.00 (¥806.00 Overspent)\r\n" + 
                 		"Origin category: 1) Unknown(¥0.00) - Est. ¥0.00 (¥0.00 Remaining)\r\n" + 
-                		"\r\n" + 
-                		"What do you want to do?\r\n" + 
+                		"\n" + 
+                		"What do you want to do?\n" + 
                 		" O = [O]verview, T = List All [T]ransactions, [num] = Show Category [num], C = [C]hange Transaction Category, A = [A]dd Transaction, N = [N]ew Category, X = E[x]it\r\n" + 
                 		"Goodbye!\r\n")
         );
