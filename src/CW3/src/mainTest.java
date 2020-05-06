bb3395acba29f30ce13eff1ede19af3b9596ce53
JiaweiWu <scyjw2@nottingham.edu.cn>
2020-05-06 22:04:48 +0800

    modify test code

--- src/CW3/src/mainTest.java
+++ src/CW3/src/mainTest.java
@@ -78,7 +78,67 @@ class mainTest {
                 		"What do you want to do?\n" + 
                 		"O = [O]verview\nT = List All [T]ransactions\n[num] = Show Category [num]\nC = [C]hange Transaction Category\nA = [A]dd Transaction\nN = [N]ew Category\nX = E[x]it\r\n" + 
                 		"Goodbye!\r\n" 
-                		)
+                		),
+                Arguments.arguments("A\nTestTitle\n6.23\nX\n",
+                		"1) Unknown(¥0.00) - Est. ¥850.00 (¥850.00 Overspent)\r\n" + 
+                		"2) Bills(¥120.00) - Est. ¥112.99 (¥7.01 Remaining)\r\n" + 
+                		"3) Groceries(¥75.00) - Est. ¥31.00 (¥44.00 Remaining)\r\n" + 
+                		"4) Social(¥100.00) - Est. ¥22.49 (¥77.51 Remaining)\r\n" + 
+                		"\n" + 
+                		"What do you want to do?\n" + 
+                		"O = [O]verview\n" + 
+                		"T = List All [T]ransactions\n" + 
+                		"[num] = Show Category [num]\n" + 
+                		"C = [C]hange Transaction Category\n" + 
+                		"A = [A]dd Transaction\n" + 
+                		"N = [N]ew Category\n" + 
+                		"X = E[x]it\r\n" + 
+                		"What is the title of the transaction?\r\n" + 
+                		"NOTE: It should not be blank and less than 25 characters.\r\n" + 
+                		"What is the value of the transaction?\r\n" + 
+                		"NOTE: It should be greater than 0 with two decimal places e.g. 10.00.\r\n" + 
+                		"What is the category of the transaction?\r\n" + 
+                		"Note: It should be the index number of a categoryType from above. Type \"1\" or press enter for the Unknown category.\r\n" + 
+                		"Something went wrong: java.lang.Exception: Wrong category. It should be an integer between 1 - 4\n" + 
+                		"\r\n" + 
+                		"\n" + 
+                		"What do you want to do?\n" + 
+                		"O = [O]verview\n" + 
+                		"T = List All [T]ransactions\n" + 
+                		"[num] = Show Category [num]\n" + 
+                		"C = [C]hange Transaction Category\n" + 
+                		"A = [A]dd Transaction\n" + 
+                		"N = [N]ew Category\n" + 
+                		"X = E[x]it\r\n"),
+               Arguments.arguments("O\nX\n",
+            		"1) Unknown(¥0.00) - Est. ¥850.00 (¥850.00 Overspent)\r\n" + 
+               		"2) Bills(¥120.00) - Est. ¥112.99 (¥7.01 Remaining)\r\n" + 
+               		"3) Groceries(¥75.00) - Est. ¥31.00 (¥44.00 Remaining)\r\n" + 
+               		"4) Social(¥100.00) - Est. ¥22.49 (¥77.51 Remaining)\r\n" + 
+               		"\n" + 
+               		"What do you want to do?\n" + 
+               		"O = [O]verview\n" + 
+               		"T = List All [T]ransactions\n" + 
+               		"[num] = Show Category [num]\n" + 
+               		"C = [C]hange Transaction Category\n" + 
+               		"A = [A]dd Transaction\n" + 
+               		"N = [N]ew Category\n" + 
+               		"X = E[x]it\r\n" + 
+               		"1) Unknown(¥0.00) - Est. ¥850.00 (¥850.00 Overspent)\r\n" + 
+               		"2) Bills(¥120.00) - Est. ¥112.99 (¥7.01 Remaining)\r\n" + 
+               		"3) Groceries(¥75.00) - Est. ¥31.00 (¥44.00 Remaining)\r\n" + 
+               		"4) Social(¥100.00) - Est. ¥22.49 (¥77.51 Remaining)\r\n" + 
+               		"\n" + 
+               		"What do you want to do?\n" + 
+               		"O = [O]verview\n" + 
+               		"T = List All [T]ransactions\n" + 
+               		"[num] = Show Category [num]\n" + 
+               		"C = [C]hange Transaction Category\n" + 
+               		"A = [A]dd Transaction\n" + 
+               		"N = [N]ew Category\n" + 
+               		"X = E[x]it\r\n" + 
+               		"Goodbye!\r\n")
+
 //                Arguments.arguments(""),
 //                Arguments.arguments("")
         );
