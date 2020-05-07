3942a58ff543957bc7e02bb9fa81df75f9f11daa
Daze_2 <scyhp1@nottingham.edu.cn>
2020-05-07 13:59:28 +0800

    Last Test of Main Update

--- src/CW3/src/BoCAppTest.java
+++ src/CW3/src/BoCAppTest.java
@@ -314,7 +314,7 @@ class BoCAppTest {
                 Change:/
                 Traceability: BoCAppmainTest5
                 */
-                Arguments.arguments("O\nt\nT\n1\nX\n",
+                Arguments.arguments("O\nt\nT\n1\nC\n1\n3\nA\nTestTitle\n6.23\n1\nN\nTestTitle\n6.23\nX\n",
                 				"1) Unknown(¥0.00) - Est. ¥850.00 (¥850.00 Overspent)\r\n" +
                                 "2) Bills(¥120.00) - Est. ¥112.99 (¥7.01 Remaining)\r\n" +
                                 "3) Groceries(¥75.00) - Est. ¥31.00 (¥44.00 Remaining)\r\n" +
@@ -378,6 +378,55 @@ class BoCAppTest {
                                 "A = [A]dd Transaction\n" +
                                 "N = [N]ew Category\n" +
                                 "X = E[x]it\r\n" +
+                                "Which transaction ID?\r\n" + 
+                        		"	- "+ sd+
+                        		" Rent - ¥850.00\r\n" + 
+                        		"Which category will it move to?\r\n" + 
+                        		"1) Unknown(¥0.00) - Est. ¥850.00 (¥850.00 Overspent)\r\n" + 
+                        		"2) Bills(¥120.00) - Est. ¥112.99 (¥7.01 Remaining)\r\n" + 
+                        		"3) Groceries(¥75.00) - Est. ¥31.00 (¥44.00 Remaining)\r\n" + 
+                        		"4) Social(¥100.00) - Est. ¥22.49 (¥77.51 Remaining)\r\n" + 
+                        		"Change complete!\r\n" + 
+                        		"Target category: 3) Groceries(¥75.00) - Est. ¥881.00 (¥806.00 Overspent)\r\n" + 
+                        		"Origin category: 1) Unknown(¥0.00) - Est. ¥0.00 (¥0.00 Remaining)\r\n" +                         	
+                                "\n" +
+                                "What do you want to do?\n" +
+                                "O = [O]verview\n" +
+                                "T = List All [T]ransactions\n" +
+                                "[num] = Show Category [num]\n" +
+                                "C = [C]hange Transaction Category\n" +
+                                "A = [A]dd Transaction\n" +
+                                "N = [N]ew Category\n" +
+                                "X = E[x]it\r\n" +
+                                "What is the title of the transaction?\r\n" +
+                                "NOTE: It should not be blank and less than 25 characters.\r\n" +
+                                "What is the value of the transaction?\r\n" +
+                                "NOTE: It should be greater than 0 with two decimal places e.g. 10.00.\r\n" +
+                                "What is the category of the transaction?\r\n" +
+                                "Note: It should be the index number of a categoryType from above. Type \"1\" or press enter for the Unknown category.\r\n" +
+                                "TestTitle(¥6.23) was added to Unknown\r\n" +
+                                "\n" +
+                                "What do you want to do?\n" +
+                                "O = [O]verview\nT = List All [T]ransactions\n[num] = Show Category [num]\nC = [C]hange Transaction Category\nA = [A]dd Transaction\nN = [N]ew Category\nX = E[x]it\r\n" +
+                                "What is the title of the category?\r\n" +
+                                "NOTE: It should not be blank and should be at most 15 characters.\r\n" +
+                                "What is the budget for this category?\r\n" +
+                                "Note:It should be a pisitive decimal number with exact two decimal places.\r\n" +
+                                "[Category added]\r\n" +
+                                "1) Unknown(¥0.00) - Est. ¥0.00 (¥0.00 Remaining)\r\n" +
+                                "2) Bills(¥120.00) - Est. ¥112.99 (¥7.01 Remaining)\r\n" +
+                                "3) Groceries(¥75.00) - Est. ¥881.00 (¥806.00 Overspent)\r\n" +
+                                "4) Social(¥100.00) - Est. ¥22.49 (¥77.51 Remaining)\r\n" +
+                                "5) TestTitle(¥6.23) - Est. ¥0.00 (¥6.23 Remaining)\r\n" +
+                                "\n" +
+                                "What do you want to do?\n" +
+                                "O = [O]verview\n" +
+                                "T = List All [T]ransactions\n" +
+                                "[num] = Show Category [num]\n" +
+                                "C = [C]hange Transaction Category\n" +
+                                "A = [A]dd Transaction\n" +
+                                "N = [N]ew Category\n" +
+                                "X = E[x]it\r\n" +
                                 "Goodbye!\r\n"
                 		)
 
@@ -406,7 +455,8 @@ class BoCAppTest {
     }
     static List<Arguments> mainTest5() {
         return List.of( // arguments:
-                Arguments.arguments("C\n1\n3\nX\n","1) Unknown(¥0.00) - Est. ¥850.00 (¥850.00 Overspent)\r\n" + 
+                Arguments.arguments("C\n1\n3\nX\n",
+                		"1) Unknown(¥0.00) - Est. ¥850.00 (¥850.00 Overspent)\r\n" + 
                 		"2) Bills(¥120.00) - Est. ¥112.99 (¥7.01 Remaining)\r\n" + 
                 		"3) Groceries(¥75.00) - Est. ¥31.00 (¥44.00 Remaining)\r\n" + 
                 		"4) Social(¥100.00) - Est. ¥22.49 (¥77.51 Remaining)\r\n" + 
